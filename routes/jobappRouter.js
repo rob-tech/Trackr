a73bff6ea0860b846a8d22ca856292a929e43d3b
rob <robertahuber88@gmail.com>
2019-12-10 18:52:26 +0100

    moving FE fetch modifications to backend

--- routes/jobappRouter.js
+++ routes/jobappRouter.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const jobApp = require("../models/jobApp")
-
-
+// const fs = require("fs")
+const multer = require("multer")
+const path = require("path");
+const fs = require("fs-extra")
 // require('dotenv').config()
 
 const router = express.Router();
@@ -11,11 +13,66 @@ router.get("/", async (req, res) => {
     console.log(res)
 })
 
+router.get("/totApp", async (req, res) => {
+    var totApp = (await jobApp.find({})).length
+    res.send({ totApp: totApp})
+})
+
+router.get("/AppsWeek", async (req, res) => {
+     var curr = new Date() 
+    var week = []
+      
+      for (let i = 1; i <= 7; i++) {
+        let first = curr.getDate() - curr.getDay() + i 
+        let day = new Date(curr.setDate(first)).toISOString().slice(0, 10)
+        week.push(day)
+        
+      }
+      var finalArr=[]
+
+      var newApplications = await jobApp.find({ status: { $in: 'applied'} })  
+    //   console.log(newApplications)
+      newApplications.forEach((e1)=>week.forEach((e2)=>{
+    //  console.log(e1.createdAt)
+        if(e1.createdAt.toISOString().slice(0, 10) == e2)
+        {
+            console.log(e2)
+            console.log(e1.createdAt)
+            finalArr.push(e1)
+        }
+      }))
+      console.log(finalArr)
+      var lastWeek = finalArr.length
+      res.send({ lastWeek: lastWeek})
+
+
+})
+
 // router.get("/", async (req, res) => {
 // 	const result = await jobApp.find({studentId:new mongoose.Types.ObjectId
 // (req.query.studentId)})
 //     res.send(result)
 // })
+var multerInstance = new multer({})
+
+router.post("/:id/image", multerInstance.single("image"), async (req, res) => {
+    console.log("hey")
+    //1) save the picture
+    var fullUrl = req.protocol + "://" + req.get("host") + "/img/"
+    console.log("url" + fullUrl)
+    var ext = path.extname(req.file.originalname)
+    var productID = req.params.id
+    var fileName = productID + ext;
+    await fs.writeFile("./images/" + fileName, req.file.buffer)
+
+    //2) update image link
+    var products = await getProducts();
+    var toUpdate = products.find(x => x.ID == req.params.id)
+    toUpdate.Image = fullUrl + fileName
+    await saveProducts(products)
+
+    res.send(toUpdate)
+}) 
 
 router.post("/", async (req, res, next) => {
     // req.body.userId = req.user._id
@@ -78,6 +135,11 @@ router.put("/:appId",
             .catch(error => next(error));
     }
 )
+
+
+
+
+
 
 
 module.exports = router;
